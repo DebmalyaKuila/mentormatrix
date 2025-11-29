ba662d31e0af49b8acf37da42dfca8b4dba99e44
scaryCoder <debmalyakuila911@gmail.com>
2025-11-30 01:03:28 +0530

    initial commit

---
+++ src/main/java/com/mentormatrix/mentormatrix/MentormatrixApplication.java
@@ -0,0 +1,13 @@
+package com.mentormatrix.mentormatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentormatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentormatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentormatrix/mentormatrix/MentormatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentormatrix.mentormatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentormatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
